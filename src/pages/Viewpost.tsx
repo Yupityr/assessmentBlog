91d9401f8281bce4f397f0df485a6d73d81b47c4
Yupityr <jrchchrstngithub@gmail.com>
2026-04-27 11:47:22 +0800

    change: viewing post now shows original poster info

--- src/pages/Viewpost.tsx
+++ src/pages/Viewpost.tsx
@@ -6,46 +6,67 @@ import { useAppSelector,useAppDispatch } from "@/app/store";
 import { useEffect } from "react";
 import { fetchPostsById } from "@/features/postsSlice";
 import Comments from "@/components/posts/Comments";
+import Loader from "@/components/common/Loader";
 
 const Viewpost = () => {
     const dispatch = useAppDispatch()
     const params = useParams()
 
-    const blogs = useAppSelector((state) => state.posts.posts.find(p => p.post_id === params.postId))
+    const {posts} = useAppSelector((state) => state.posts)
 
 
     useEffect(() => {
         const postId = params.postId
         if (postId){
             dispatch(fetchPostsById(postId))
+            console.log();
+            
         }
     },[dispatch, params.postId])
 
 
     return (
         <>
-        <article className="mx-auto p-4">
-            <div className="border-b border-gray-300 mb-4">
-                <p className="text-center text-3xl mb-4 pb-4">
-                    {blogs?.title}
-                </p>
-            </div>
-            <div className="min-h-full">
-                {blogs?.body && 
-                <>
-                <Readonlyeditor postContent={blogs?.body}/>
-                <div className="mx-auto p-auto">
-                    <section className="text-[8px] sm:text-xs items-start font-bold sm:px-5">
-                        <h1 className=" header-text my-2">Comments</h1>
-                    </section>
-                    <Comments postId={blogs?.post_id} />
+        {posts.map(blogs => (
+            <>
+            {!blogs.profiles && <Loader /> }
+            {blogs.profiles &&
+            <article className="mx-auto p-4">
+                <div className="border-b border-gray-300 mb-4">
+                    <p className="text-center text-3xl mb-4 pb-4">
+                        {blogs?.title}
+                    </p>
+                    <div className="flex justify-between">
+                        <div className="flex flex-row gap-1">
+                                <img
+                                    src={blogs?.profiles?.avatar_url ?? "https://wxlokatxrwhwpyvgcpre.supabase.co/storage/v1/object/public/blogBucket/output-onlinepngtools.png"}
+                                    alt="Profile"
+                                    className="h-9 w-9 rounded-full border"
+                                />
+                                <span className="m-auto">
+                                {blogs?.profiles.username}
+                                </span>
+                        </div>
+                        {new Date(blogs.created_at).toLocaleString()}
+                    </div>
                 </div>
-                </>
-                }
-                {/* commment section */}
-                
-            </div>
-        </article>
+                <div className="min-h-full">
+                    {blogs?.body && 
+                    <>
+                    <Readonlyeditor postContent={blogs?.body}/>
+                    <div className="mx-auto p-auto">
+                        <section className="text-[8px] sm:text-xs items-start font-bold sm:px-5">
+                            <h1 className=" header-text my-2">Comments</h1>
+                        </section>
+                        <Comments postId={blogs?.post_id} />
+                    </div>
+                    </>
+                    }
+                    {/* commment section */}
+                    
+                </div>
+            </article>}</>
+        ))}
         </>
     );
 };
